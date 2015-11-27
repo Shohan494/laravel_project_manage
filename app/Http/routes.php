6a77f68458483653a3e1d892d7909b3357d221d2
Shohanul Alam <shohan12224943@gmail.com>
2015-11-27 10:21:03 +0600

    collaborator add route added, collaborator form edited
    form edited: According to the package documentation, users refers to the
    users table and username refer to the column of the users table. That’s
    where we want the auto-suggest data to come from.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -92,3 +92,10 @@ Route::put('projects/{projects}/comments/{comments}', [
 Route::delete('projects/{projects}/comments/{comments}', [
     'uses' => 'ProjectCommentsController@deleteOneProjectComment',
 ]);
+
+# Collaborator routes
+Route::post('projects/{projects}/collaborators', [
+    'uses' => 'ProjectCollaboratorsController@addCollaborator',
+    'as'   => 'projects.collaborators.create',
+    'middleware' => ['auth']
+]);
