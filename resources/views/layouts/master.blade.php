0bfd2644a91cd369afbbbfddd436778ae581f32d
Shohanul Alam <shohan12224943@gmail.com>
2015-11-27 10:13:38 +0600

    this command created some files, edited the namspace of mentions.php, master template is edited
    php artisan vendor:publish

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,6 +5,13 @@
     <title>Prego - Project Management App</title>
     <meta name="description" content="Prego is a project management app built for learning purposes">
     <link rel="stylesheet" href="{{ asset('css/dashboard.css') }}">
+
+    <script src="{{ asset('js/jquery.atwho.js') }}"></script>
+    <script src="{{ asset('js/jquery.caret.js') }}"></script>
+ 
+    <link rel="stylesheet" href="{{ asset('css/jquery.atwho.min.css') }}">
+    
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.js"></script>
     <script src="{{ asset('js/app.js') }}"></script>
  
@@ -18,6 +25,8 @@
     <div class="container">
         @include('layouts.partials.alerts')
         @yield('content')
+
+        @include('mentions::assets')
     </div>
 </body>
 </html>
